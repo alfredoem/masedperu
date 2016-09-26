e78d686b520131666ce4d61d9bb3b07bd13a57bb
Alfredo EM <alfredo.espiritu.m@gmail.com>
2016-09-26 12:08:12 -0500

    Header refactor

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,51 +1,40 @@
 <header>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3 col-md-offset-1">
-                <img src="{{asset('images/home/logo2.png')}}" alt="Logo" class="img-responsive">
-            </div>
-            <div class="col-md-8">
-                <div class="row">
-                    <div class="col-md-4 nopadding">
-                        <div class="row">
-                            <div class="col-md-1 nopadding">
-                                <span class="fa fa-phone fa-2x"></span>
-                            </div>
-                            <div class="col-md-11 nopadding">
-                                <span class="bold">(01) 423-3900</span>
-                                <br/>
-                                infoventas@masedperu.com
-                            </div>
-                        </div>
+    <div class="container container-header">
+        <div class="logo">
+            <img src="{{asset('images/home/logo2.png')}}" alt="Logo" class="img-responsive">
+        </div>
+        <div class="menu">
+            <div class="container-menu">
+                <div class="item-menu">
+                    <div class="icon-menu">
+                        <span class="fa fa-phone fa-3x"></span>
                     </div>
-                    <div class="col-md-4 nopadding">
-                        <div class="row">
-                            <div class="col-md-1 nopadding">
-                                <span class="fa fa-clock-o fa-2x"></span>
-                            </div>
-                            <div class="col-md-6 nopadding">
-                                <span class="bold">Manuel Corpancho 212</span>
-                                <br/>
-                                Lima, Perú
-                            </div>
-                        </div>
+                    <div class="text-menu">
+                        <span class="bold">(01) 423-3900</span>
+                        <p>infoventas@masedperu.com</p>
                     </div>
-                    <div class="col-md-4 nopadding">
-                        <div class="row">
-                            <div class="col-md-1 nopadding">
-                                <span class="fa fa-home fa-2x"></span>
-                            </div>
-                            <div class="col-md-11 nopadding">
-                                <span class="bold">L - V 8:30 am - 06:00 pm</span>
-                                <br/>
-                                Sábado - Domingo CERRADO
-                            </div>
-                        </div>
+                </div>
+                <div class="item-menu">
+                    <div class="icon-menu">
+                        <span class="fa fa-home fa-3x"></span>
                     </div>
-                    <div class="col-md-1 nopadding">
-                        <div class="row">
-                            <i class="fa fa-facebook fa-2x"></i>
-                        </div>
+                    <div class="text-menu">
+                        <span class="bold">Manuel Corpancho 212</span>
+                        <p>Lima, Perú</p>
+                    </div>
+                </div>
+                <div class="item-menu">
+                    <div class="icon-menu">
+                        <span class="fa fa-clock-o fa-3x"></span>
+                    </div>
+                    <div class="text-menu">
+                        <span class="bold">L - V 8:30 am - 06:00 pm</span>
+                        <p>Sábado - Domingo CERRADO</p>
+                    </div>
+                </div>
+                <div class="item-menu">
+                    <div class="icon-menu">
+                        <i class="fa fa-facebook fa-3x"></i>
                     </div>
                 </div>
             </div>
